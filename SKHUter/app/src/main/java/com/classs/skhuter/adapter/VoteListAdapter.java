80c9c69cd278706a3ef659734b4aa1b2c5156711
JONG-YOON <jongyoon9515@gmail.com>
2017-10-07 18:46:43 +0900

    투표 리스트뷰 + 신호등이미지

---
+++ SKHUter/app/src/main/java/com/classs/skhuter/adapter/VoteListAdapter.java
@@ -0,0 +1,86 @@
+package com.classs.skhuter.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.classs.skhuter.R;
+import com.classs.skhuter.domain.UserDTO;
+
+import java.util.List;
+
+/**
+ * 학생 목록 어댑터 - 리스트뷰
+ *
+ * @패키지 : com.classs.skhuter.adapter
+ * @파일명 : VoteListAdapter.java
+ * @작성자 : 이종윤
+ * @작성일 : 2017. 10. 07
+ *
+ */
+
+public class VoteListAdapter extends BaseAdapter {
+    Context context;
+    int layout;
+    List<UserDTO> userDTOList;
+    LayoutInflater inflater;
+
+    public VoteListAdapter(Context context, int layout, List<UserDTO> restaList) {
+        this.context = context;
+        this.layout = layout;
+        this.userDTOList = restaList;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    } // Constructor
+
+    @Override
+    public int getCount() {
+        return userDTOList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return userDTOList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.item_user_list, parent, false);
+
+            viewHolder = new ViewHolder();
+            viewHolder.ivProfile = (ImageView) convertView.findViewById(R.id.ivProfile);
+            viewHolder.tvUserName = (TextView) convertView.findViewById(R.id.tvUserName);
+            viewHolder.tvUserSSN = (TextView) convertView.findViewById(R.id.tvUserSSN);
+            viewHolder.tvUserGrade = (TextView) convertView.findViewById(R.id.tvUserGrade);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        UserDTO user = (UserDTO) getItem(position);
+        //Glide.with(context).load("http://ldh66210.cafe24.com/upload/"+nBean.getImgPath()).thumbnail(0.1f).error(R.drawable.ic_group).into(viewHolder.ivNewsImg);
+        viewHolder.tvUserName.setText(user.getName());
+        viewHolder.tvUserSSN.setText(user.getId());
+        viewHolder.tvUserGrade.setText(user.getGrade() + "학년");
+        return convertView;
+    }
+
+    static class ViewHolder {
+        public ImageView ivProfile;
+        public TextView tvUserName;
+        public TextView tvUserSSN;
+        public TextView tvUserGrade;
+    } // end of inner class ViewHolder
+} // end of class
---
+++ SKHUter/app/src/main/java/com/classs/skhuter/domain/VoteDTO.java
@@ -0,0 +1,168 @@
+package com.classs.skhuter.domain;
+import java.util.Date;
+
+/**
+ * 투표 관련 객체 선언 및 getter(), setter() 메소드 정의
+ *
+ * @패키지 : com.classs.skhuter.user.domain
+ * @파일명 : UserDTO.java
+ * @작성자 : 이종윤
+ * @작성일 : 2017. 10. 07
+ *
+ */
+public class VoteDTO {
+	
+	private int voteNo;
+	private int userNo;
+	private Date enrollDate;
+	private String startDate;
+	private String endDate;
+	private String title;
+	private String content;
+	private String item1;
+	private String item2;
+	private String item3;
+	private String item4;
+	private String item5;
+	private String item6;
+	private int item1Count;
+	private int item2Count;
+	private int item3Count;
+	private int item4Count;
+	private int item5Count;
+	private int item6Count;
+	private int isVote;
+	
+	
+	public int getIsVote() {
+		return isVote;
+	}
+	public void setIsVote(int isVote) {
+		this.isVote = isVote;
+	}
+	public int getItem1Count() {
+		return item1Count;
+	}
+	public void setItem1Count(int item1Count) {
+		this.item1Count = item1Count;
+	}
+	public int getItem2Count() {
+		return item2Count;
+	}
+	public void setItem2Count(int item2Count) {
+		this.item2Count = item2Count;
+	}
+	public int getItem3Count() {
+		return item3Count;
+	}
+	public void setItem3Count(int item3Count) {
+		this.item3Count = item3Count;
+	}
+	public int getItem4Count() {
+		return item4Count;
+	}
+	public void setItem4Count(int item4Count) {
+		this.item4Count = item4Count;
+	}
+	public int getItem5Count() {
+		return item5Count;
+	}
+	public void setItem5Count(int item5Count) {
+		this.item5Count = item5Count;
+	}
+	public int getItem6Count() {
+		return item6Count;
+	}
+	public void setItem6Count(int item6Count) {
+		this.item6Count = item6Count;
+	}
+	
+	
+	
+	public int getVoteNo() {
+		return voteNo;
+	}
+	public void setVoteNo(int voteNo) {
+		this.voteNo = voteNo;
+	}
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	public Date getEnrollDate() {
+		return enrollDate;
+	}
+	public void setEnrollDate(Date enrollDate) {
+		this.enrollDate = enrollDate;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getItem1() {
+		return item1;
+	}
+	public void setItem1(String item1) {
+		this.item1 = item1;
+	}
+	public String getItem2() {
+		return item2;
+	}
+	public void setItem2(String item2) {
+		this.item2 = item2; 
+	}
+	public String getItem3() {
+		return item3;
+	}
+	public void setItem3(String item3) {
+		this.item3 = item3;
+	}
+	public String getItem4() {
+		return item4;
+	}
+	public void setItem4(String item4) {
+		this.item4 = item4;
+	}
+	public String getItem5() {
+		return item5;
+	}
+	public void setItem5(String item5) {
+		this.item5 = item5;
+	}
+	public String getItem6() {
+		return item6;
+	}
+	public void setItem6(String item6) {
+		this.item6 = item6;
+	}
+	@Override
+	public String toString() {
+		return "VoteDTO [voteNo=" + voteNo + ", userNo=" + userNo + ", enrollDate=" + enrollDate + ", startDate="
+				+ startDate + ", endDate=" + endDate + ", title=" + title + ", content=" + content + ", item1=" + item1
+				+ ", item2=" + item2 + ", item3=" + item3 + ", item4=" + item4 + ", item5=" + item5 + ", item6=" + item6
+				+ ", item1Count=" + item1Count + ", item2Count=" + item2Count + ", item3Count=" + item3Count
+				+ ", item4Count=" + item4Count + ", item5Count=" + item5Count + ", item6Count=" + item6Count + "]";
+	}
+}
