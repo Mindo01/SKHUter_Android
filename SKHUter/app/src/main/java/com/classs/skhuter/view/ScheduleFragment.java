158066cbcfae7fe6960abc222712b71b948a793e
LG\김민도 <y229600@naver.com>
2017-10-08 14:33:25 +0900

    학생회 일정 + 학사 일정 뷰 완성 - 커스텀 라이브러리 : material-calendar-view 사용 출처 : http://applandeo.com/blog/material-calendar-view-customized-calendar-widget-android/

--- SKHUter/app/src/main/java/com/classs/skhuter/view/ScheduleFragment.java
+++ SKHUter/app/src/main/java/com/classs/skhuter/view/ScheduleFragment.java
@@ -1,17 +1,26 @@
 package com.classs.skhuter.view;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 import com.classs.skhuter.R;
 
-public class ScheduleFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
+public class ScheduleFragment extends Fragment {
+    private CalendarView mCalendarView;
+    private List<EventDay> mEventDays = new ArrayList<>();
+    private TextView tvSelectedDay;
     public ScheduleFragment() {
         // Required empty public constructor
     }
@@ -24,7 +33,41 @@ public class ScheduleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        View v = inflater.inflate(R.layout.fragment_schedule, container, false);
+
+        mCalendarView = (CalendarView)v.findViewById(R.id.datePicker);
+        tvSelectedDay = (TextView)v.findViewById(R.id.tvToday);
+
+        Calendar cal = Calendar.getInstance();
+        Log.e("DateHere", cal.get(Calendar.YEAR)+"-"+(cal.get(Calendar.MONTH)+1)+"-"+cal.get(Calendar.DATE));
+        // 이벤트 목록에 추가
+        mEventDays.add(new EventDay(cal, R.drawable.ic_meeting_note));
+        mEventDays.add(new EventDay(cal, R.drawable.ic_meeting_note));
+        // 이벤트 목록을 달력에 등록
+        mCalendarView.setEvents(mEventDays);
+
+        // 일자 클릭 시 이벤트 실행
+        mCalendarView.setOnDayClickListener(new OnDayClickListener() {
+            @Override
+            public void onDayClick(EventDay eventDay) {
+                Log.e("onDay", "날짜 누름");
+                // 선택된 날짜 받아오기
+                Calendar selectedDay = eventDay.getCalendar();
+                String year = selectedDay.get(Calendar.YEAR)+"";
+                String month = selectedDay.get(Calendar.MONTH)+1+"";
+                if (month.length() < 2) {
+                    month = "0"+month;
+                }
+                String day = selectedDay.get(Calendar.DATE)+"";
+                if (day.length() < 2) {
+                    day = "0"+day;
+                }
+                // 선택된 날짜 표기하기
+                tvSelectedDay.setText(year+"-"+month+"-"+day);
+                // 선택된 날짜의 이벤트 표시해주기
+            }
+        });
+
+        return v;
     }
 }
--- SKHUter/app/src/main/java/com/classs/skhuter/view/StuScheduleFragment.java
+++ SKHUter/app/src/main/java/com/classs/skhuter/view/StuScheduleFragment.java
@@ -2,14 +2,25 @@ package com.classs.skhuter.view;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 import com.classs.skhuter.R;
 
-public class StuScheduleFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
+public class StuScheduleFragment extends Fragment {
+    private CalendarView mCalendarView;
+    private List<EventDay> mEventDays = new ArrayList<>();
+    private TextView tvSelectedDay;
     public StuScheduleFragment() {
         // Required empty public constructor
     }
@@ -22,7 +33,41 @@ public class StuScheduleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stu_schedule, container, false);
+        View v = inflater.inflate(R.layout.fragment_stu_schedule, container, false);
+
+        mCalendarView = (CalendarView)v.findViewById(R.id.stu_calendar);
+        tvSelectedDay = (TextView)v.findViewById(R.id.tvToday);
+
+        Calendar cal = Calendar.getInstance();
+        Log.e("DateHere", cal.get(Calendar.YEAR)+"-"+(cal.get(Calendar.MONTH)+1)+"-"+cal.get(Calendar.DATE));
+        // 이벤트 목록에 추가
+        mEventDays.add(new EventDay(cal, R.drawable.ic_meeting_note));
+        mEventDays.add(new EventDay(cal, R.drawable.ic_meeting_note));
+        // 이벤트 목록을 달력에 등록
+        mCalendarView.setEvents(mEventDays);
+
+        // 일자 클릭 시 이벤트 실행
+        mCalendarView.setOnDayClickListener(new OnDayClickListener() {
+            @Override
+            public void onDayClick(EventDay eventDay) {
+                Log.e("onDay", "날짜 누름");
+                // 선택된 날짜 받아오기
+                Calendar selectedDay = eventDay.getCalendar();
+                String year = selectedDay.get(Calendar.YEAR)+"";
+                String month = selectedDay.get(Calendar.MONTH)+1+"";
+                if (month.length() < 2) {
+                    month = "0"+month;
+                }
+                String day = selectedDay.get(Calendar.DATE)+"";
+                if (day.length() < 2) {
+                    day = "0"+day;
+                }
+                // 선택된 날짜 표기하기
+                tvSelectedDay.setText(year+"-"+month+"-"+day);
+                // 선택된 날짜의 이벤트 표시해주기
+            }
+        });
+
+        return v;
     }
 }
