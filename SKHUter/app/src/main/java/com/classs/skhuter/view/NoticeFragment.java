de558e23dd8528a626728dcdcd3c082cd620df58
LG\김민도 <y229600@naver.com>
2017-10-09 18:47:04 +0900

    오류 고침

--- SKHUter/app/src/main/java/com/classs/skhuter/view/NoticeFragment.java
+++ SKHUter/app/src/main/java/com/classs/skhuter/view/NoticeFragment.java
@@ -28,7 +28,7 @@ public class NoticeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_board, container, false);
+        View v = inflater.inflate(R.layout.fragment_notice, container, false);
         context = getActivity().getApplicationContext();
 
         wvNotice = (WebView)v.findViewById(R.id.wvNotice);
