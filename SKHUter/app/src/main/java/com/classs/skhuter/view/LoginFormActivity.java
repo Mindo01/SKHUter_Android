5e75a8e608e9406f42e7ac4a4c5086e3b6c306d4
JONG-YOON <jongyoon9515@gmail.com>
2017-10-09 22:58:00 +0900

    1. 회원가입 페이지 디자인 수정 2. 로그인 기능 구현

--- SKHUter/app/src/main/java/com/classs/skhuter/view/LoginFormActivity.java
+++ SKHUter/app/src/main/java/com/classs/skhuter/view/LoginFormActivity.java
@@ -3,13 +3,26 @@ package com.classs.skhuter.view;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.classs.skhuter.R;
+import com.classs.skhuter.domain.UserDTO;
 import com.classs.skhuter.util.Connection;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginFormActivity extends Activity {
 
@@ -22,11 +35,7 @@ public class LoginFormActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
 
-        //TODO 로그인 정보 임의로 설정
-        Connection.loginUser.setUserNo(2);
-        Connection.loginUser.setName("김민주");
-        Connection.loginUser.setGrade(4);
-        Connection.loginUser.setId("201334005");
+        Connection.loginUser = null;
 
         edtID = (EditText)findViewById(R.id.edtID);
         edtPW = (EditText)findViewById(R.id.edtPW);
@@ -37,12 +46,20 @@ public class LoginFormActivity extends Activity {
             public void onClick(View v) {
                 // 로그인
                 // TODO 바꾸자
+                UserDTO user = new UserDTO();
                 if (edtID.getText().toString().length() > 0) {
-                    Connection.loginUser.setUserNo(Integer.valueOf(edtID.getText().toString()));
+                    user.setUserNo(Integer.valueOf(edtID.getText().toString()));
+                    user.setId(edtID.getText().toString());
+
                 }
-                Intent intent = new Intent(LoginFormActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+
+                if (edtPW.getText().toString().length() > 0) {
+                    user.setPassword(edtPW.getText().toString());
+                }
+
+                sendLoginResult(user);
+
+
             }
         });
 
@@ -68,5 +85,63 @@ public class LoginFormActivity extends Activity {
             }
         });
 
+    }
+
+    /**
+     * 회원가입을 Volley로 데이터 보내는 메소드
+     */
+    void sendLoginResult(UserDTO user) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String PARAMS = "?id="+ user.getId()
+                +"&password="+user.getPassword();
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, Connection.ADDRESS+Connection.GET_Login+PARAMS, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject obj) {
+
+                        Gson gson = new Gson();
+                        JSONObject result = null;
+                        try {
+                            result = obj.getJSONObject("user");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        if (result == null) {
+                            // 로그인 실패
+                            Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // 로그인 성공
+                            // 로그인 정보 확인
+                            Connection.loginUser = gson.fromJson(result.toString(), UserDTO.class);
+
+                            Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
+                            // 로그인 정보 저장
+                            Log.e("Login USER", Connection.loginUser.getName()+", "+Connection.loginUser.getUserNo()+", "+Connection.loginUser.getId());
+                            // 홈 화면으로 이동
+                            Intent intent = new Intent(LoginFormActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.d("MyLog", "error : " + error);
+                final VolleyError err = error;
+
+/*                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("MyLog", "error : " + err);
+                        tvError.setText("네트워크 연결이 원활하지 않습니다");
+                        tvError.setVisibility(View.VISIBLE);
+                    }
+                });*/
+            }
+        });
+        requestQueue.add(request);
     }
 }
